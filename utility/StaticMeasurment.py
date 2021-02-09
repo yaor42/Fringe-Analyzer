ce1c776ba949a030c2a1d6b738ba821947d7c777
ResponsoMundus <gamesluook_hi@126.com>
2021-02-09 13:52:13 -0600

    Update StaticMeasurment.py

--- utility/StaticMeasurment.py
+++ utility/StaticMeasurment.py
@@ -4,8 +4,8 @@ from utility.FringeAnalysisFunctions import *
 ks = 1  # factor from phase to depth
 
 # Read in reference and object images
-refImg = cv2.imread('../TestPics/cone3-r.jpg', 0)
-objImg = cv2.imread('../TestPics/cone3.jpg', 0)
+refImg = cv2.imread('../TestPics/cone2-r.jpg', 0)
+objImg = cv2.imread('../TestPics/cone2.jpg', 0)
 
 # plt.imshow(refImg, cmap='gray')
 # plt.show()
@@ -35,12 +35,54 @@ depthMap = unwrappedPhaseMap * ks
 # plt.colorbar()
 # plt.show()
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-X = np.array([[x for x in range(0, 1000)] for i in range(0, 1000)])
-Y = np.array([[x for i in range(0, 1000)] for x in range(0, 1000)])
-surf = ax.plot_surface(X, Y, depthMap, rstride=50, cstride=50, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-ax.set_zlim(0, 80)
-fig.colorbar(surf, shrink=0.5, aspect=5)
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# X = np.array([[x for x in range(0, 1000)] for i in range(0, 1000)])
+# Y = np.array([[x for i in range(0, 1000)] for x in range(0, 1000)])
+# surf = ax.plot_surface(X, Y, depthMap, rstride=50, cstride=50, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+# ax.set_zlim(0, 80)
+# fig.colorbar(surf, shrink=0.5, aspect=5)
+#
+# plt.show()
 
+max_depth = -1000
+min_depth = 1000
+
+for row in depthMap:
+    for elem in row:
+        if elem >= max_depth:
+            max_depth = elem
+        if elem <= min_depth:
+            min_depth = elem
+
+step = 256 / (max_depth - min_depth)
+
+gray = np.array([[int((elem - min_depth) * step) for elem in row] for row in depthMap])
+# gray = np.array([[80 if elem > 6 else 3 for elem in row] for row in gray])
+gray = gray.astype(np.uint8)
+gray = cv2.medianBlur(gray, 5)
+
+gray = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 7, 2)
+
+print(gray)
+
+plt.imshow(gray)
 plt.show()
+
+circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1,
+                           gray.shape[0],
+                           param1=50, param2=5,
+                           minRadius=1, maxRadius=int(gray.shape[0] / 2))
+
+if circles is not None:
+    circles = np.uint16(np.around(circles))
+    for i in circles[0, :]:
+        center = (i[0], i[1])
+        # circle center
+        cv2.circle(objImg, center, 1, (0, 100, 100), 3)
+        # circle outline
+        radius = i[2]
+        cv2.circle(objImg, center, radius, (255, 0, 255), 3)
+
+cv2.imshow("detected circles", objImg)
+cv2.waitKey(0)
